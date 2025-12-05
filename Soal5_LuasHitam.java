d94b8cdb558c3810409445985a3340ce86399b40
Riansyah <syahryan547@gmail.com>
2025-12-05 17:21:26 +0700

    Create Soal5_LuasHitam.java

---
+++ Soal5_LuasHitam.java
@@ -0,0 +1,27 @@
+class PersegiPanjang {
+    int p, l;
+
+    PersegiPanjang(int p, int l){
+        this.p = p;
+        this.l = l;
+    }
+
+    int luas(){
+        return p * l;
+    }
+}
+
+public class Soal5_LuasHitam {
+    static int hitungLuasHitam(PersegiPanjang luar, PersegiPanjang dalam){
+        return luar.luas() - dalam.luas();
+    }
+
+    public static void main(String[] args) {
+        PersegiPanjang luar = new PersegiPanjang(10, 10);
+        PersegiPanjang dalam = new PersegiPanjang(8, 5);
+
+        int hasil = hitungLuasHitam(luar, dalam);
+
+        System.out.println("Luas bidang hitam = " + hasil);
+    }
+}
