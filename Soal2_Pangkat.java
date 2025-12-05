3bf2af6cf25efbfe64e199a18003c2b382304425
Riansyah <syahryan547@gmail.com>
2025-12-05 17:18:18 +0700

    Create Soal2_Pangkat.java

---
+++ Soal2_Pangkat.java
@@ -0,0 +1,23 @@
+class Pangkat {
+    int angka, pangkat;
+
+    Pangkat(int angka, int pangkat){
+        this.angka = angka;
+        this.pangkat = pangkat;
+    }
+
+    int hitung(){
+        int hasil = 1;
+        for(int i = 0; i < pangkat; i++){
+            hasil *= angka;
+        }
+        return hasil;
+    }
+}
+
+public class Soal2_Pangkat {
+    public static void main(String[] args) {
+        Pangkat p = new Pangkat(2, 5);
+        System.out.println("Hasil pangkat = " + p.hitung());
+    }
+}
