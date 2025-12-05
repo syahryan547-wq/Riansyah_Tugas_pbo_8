c854286fc3eaa72d6e87391fe25a96c996fb5497
Riansyah <syahryan547@gmail.com>
2025-12-05 17:18:58 +0700

    Create Soal3_Biner.java

---
+++ Soal3_Biner.java
@@ -0,0 +1,25 @@
+class Biner {
+    int n;
+
+    Biner(int n){
+        this.n = n;
+    }
+
+    String konversi(){
+        String hasil = "";
+        int temp = n;
+
+        while(temp > 0){
+            hasil = (temp % 2) + hasil;
+            temp /= 2;
+        }
+        return hasil;
+    }
+}
+
+public class Soal3_Biner {
+    public static void main(String[] args) {
+        Biner b = new Biner(25);
+        System.out.println("Biner: " + b.konversi());
+    }
+}
